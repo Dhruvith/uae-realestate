374f3eff0238a84f9668f851fc75ca19f5cd9b52
dhruvith-soulax <chokkarapu.dhruvith@soulax.io>
2026-02-21 18:38:06 +0530

    chore: add video header cache control

--- next.config.js
+++ next.config.js
@@ -12,6 +12,18 @@ const nextConfig = {
             }
         ],
     },
+    async headers() {
+        return [
+            {
+                source: '/videos/:path*',
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=31536000, immutable',
+                    },
+                ],
+            },
+        ];
+    },
 };
-
 module.exports = nextConfig;
